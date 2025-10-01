3e8779cfa1141444ae16cce54cf74a9290cbd83c
renan544 <renan.rrs.santos@gmail.com>
2025-10-01 20:55:56 -0300

    Add files via upload

---
+++ exemplosjava/js/script02.js
@@ -0,0 +1,4 @@
+var numero01 = parseInt(prompt("digite um numero :"))
+var numero02=parseInt(prompt("digite outro numero :"))
+var soma = numero01 + numero02
+alert("a soma dos numeros digitados:   " + soma)
